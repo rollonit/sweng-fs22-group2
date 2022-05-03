0fd280262ba01d2201c66814a898602930429f60
rollonit <46538361+rollonit@users.noreply.github.com>
2022-05-03 09:23:08 +0200

    Reconfigured DB handler to use MariaDB
    - Also added a little test in main to check the DB.
    - I KNOW THERE'S A PLAINTEXT PASSWORD IN THERE BUT EH IT'S A PRIVATE REPO

--- src/main/java/hslu/sweng/fs22/team2/App.java
+++ src/main/java/hslu/sweng/fs22/team2/App.java
@@ -1,13 +1,27 @@
 package hslu.sweng.fs22.team2;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello World!");
+        DBHandler testHandler = new DBHandler();
+        ResultSet rs = testHandler.query("SHOW FULL TABLES;");
+        ResultSetMetaData rsmd = rs.getMetaData();
+        System.out.println("querying SELECT * FROM XXX");
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs.getString(i);
+                System.out.print(columnValue + " " + rsmd.getColumnName(i));
+            }
+            System.out.println("");
+        }
     }
 }
--- src/main/java/hslu/sweng/fs22/team2/DBHandler.java
+++ src/main/java/hslu/sweng/fs22/team2/DBHandler.java
@@ -20,10 +20,10 @@ public class DBHandler {
      * Default constructor for the DB handler, initialises the connection and makes it ready for queries. TODO check if static info and functions are better suited here.
      */
     public DBHandler() {
-        this.connectionUrl = "jdbc:postgresql://192.168.137.81:5432/sweng2"; //TODO replace with actual final DB url, may not be PostgreSQL
+        this.connectionUrl = "jdbc:mariadb://hashtagit.ch:3306/hashtag_SWENG";
         this.connectionProperties = new Properties();
-        connectionProperties.setProperty("user", "sweng2");
-        connectionProperties.setProperty("password", "sweng2pass");
+        connectionProperties.setProperty("user", "cinema_admin");
+        connectionProperties.setProperty("password", "SgWm21M6gp9S");
     }
 
 
@@ -32,15 +32,14 @@ public class DBHandler {
      *
      * @param queryText the text contents of the SQL query
      */
-    public void query(String queryText) {
-        try (Connection connection = DriverManager.getConnection(connectionUrl, connectionProperties); Statement statement = connection.createStatement();) {
-            //String testQueryTable = "insert into test" + "\nvalues(01, 'test01first', 'test01last');";
-            ResultSet resultSet = statement.executeQuery(queryText);
-            System.out.println(resultSet.toString());
+    public ResultSet query(String queryText) {
+        try (Connection sqlConnection = DriverManager.getConnection(connectionUrl, connectionProperties); Statement statement = sqlConnection.createStatement();) {
+            return statement.executeQuery(queryText);
         }
         // Handle any errors that may have occurred.
         catch (SQLException e) {
             e.printStackTrace();
+            return null;
         }
     }
 }
